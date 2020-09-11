aef92a843406131888e16f40ef37c3c17361e5f8
Marco Hijacker <marco.longo@hotmail.it>
2020-09-11 21:00:23 +0200

    Esercizio parziale (JOIN).

--- Esercizio DB Hotel - GROUP BY and JOIN.sql
+++ Esercizio DB Hotel - GROUP BY and JOIN.sql
@@ -1,31 +1,31 @@
-Studente : Marco Longo
-Esercizio Database Hotel - GROUP BY and JOIN 11.09.20 (Boolean Careers):
+-- Studente : Marco Longo
+-- Esercizio Database Hotel - GROUP BY and JOIN 11.09.20 (Boolean Careers):
 
-GOAL: 
+-- GOAL:
 
 GROUP BY
-    • Conta gli ospiti raggruppandoli per anno di nascita.
+    -- • Conta gli ospiti raggruppandoli per anno di nascita.
 Soluzione:
 ---
 SELECT YEAR(date_of_birth), COUNT(*)
 FROM ospiti
 GROUP BY YEAR(date_of_birth)
 ---
-    • Somma i prezzi dei pagamenti raggruppandoli per status.
+    -- • Somma i prezzi dei pagamenti raggruppandoli per status.
 Soluzione:
 ---
 SELECT status, SUM(price)
 FROM pagamenti
 GROUP BY status
 ---
-    • Conta quante volte è stata prenotata ogni stanza.
+    -- • Conta quante volte è stata prenotata ogni stanza.
 Soluzione:
 ---
 SELECT stanza_id, COUNT(*)
 FROM prenotazioni
 GROUP BY stanza_id
 ---
-    • Fai una analisi per vedere se ci sono ore in cui le prenotazioni sono più frequenti.
+    -- • Fai una analisi per vedere se ci sono ore in cui le prenotazioni sono più frequenti.
 Soluzione:
 ---
 SELECT HOUR(created_at), COUNT(*)
@@ -34,32 +34,49 @@ GROUP BY HOUR(created_at)
 ---
 
 JOIN
-    • Come si chiamano gli ospiti che hanno fatto più di due prenotazioni? (TRALASCIARE)
+    -- • Come si chiamano gli ospiti che hanno fatto più di due prenotazioni? (TRALASCIARE)
 Soluzione:
 ---
 
 ---
-    • Stampare tutti gli ospiti per ogni prenotazione.
+    -- • Stampare tutti gli ospiti per ogni prenotazione.
+Soluzione:
+---
+SELECT prenotazioni_has_ospiti.prenotazione_id, ospiti.name, ospiti.lastname
+FROM prenotazioni_has_ospiti
+JOIN ospiti
+ON prenotazioni_has_ospiti.ospite_id = ospiti.id
+---
+    -- • Stampare Nome, Cognome, Prezzo e Pagante per tutte le prenotazioni fatte a Maggio 2018.
+Soluzione:
+---
+SELECT  prenotazioni.id,
+        ospiti.name, ospiti.lastname,
+        pagamenti.price,
+        paganti.name, paganti.lastname, paganti.address,
+		    prenotazioni.created_at
+FROM prenotazioni_has_ospiti
+LEFT JOIN prenotazioni
+ON prenotazioni_has_ospiti.prenotazione_id = prenotazioni.id
+LEFT JOIN ospiti
+ON prenotazioni_has_ospiti.ospite_id = ospiti.id
+LEFT JOIN pagamenti
+ON pagamenti.prenotazione_id = prenotazioni.id
+LEFT JOIN paganti
+ON paganti.ospite_id = ospiti.id
+WHERE YEAR(prenotazioni.created_at) = 2018 AND MONTH(prenotazioni.created_at) = 5
+---
+    -- • Fai la somma di tutti i prezzi delle prenotazioni per le stanze del primo piano.
 Soluzione:
 ---
 
 ---
-    • Stampare Nome, Cognome, Prezzo e Pagante per tutte le prenotazioni fatte a Maggio 2018.
+    -- • Prendi i dati di fatturazione per la prenotazione con id=7.
 Soluzione:
 ---
 
 ---
-    • Fai la somma di tutti i prezzi delle prenotazioni per le stanze del primo piano.
-Soluzione:
----
-
----
-    • Prendi i dati di fatturazione per la prenotazione con id=7.
-Soluzione:
----
-
----
-    • Le stanze sono state tutte prenotate almeno una volta? (Visualizzare le stanze non ancora prenotate).
+    -- • Le stanze sono state tutte prenotate almeno una volta? (Visualizzare le stanze non ancora prenotate).
 Soluzione:
 ---
 
--- Esercizio DB Hotel.sql
+++ Esercizio DB Hotel.sql
@@ -1,37 +1,37 @@
-Studente : Marco Longo
-Esercizio Database Hotel 10.09.20 (Boolean Careers):
+-- Studente : Marco Longo
+-- Esercizio Database Hotel 10.09.20 (Boolean Careers):
+--
+-- GOAL: produrre le query per rispondere alle seguenti interrogazioni.
 
-GOAL: produrre le query per rispondere alle seguenti interrogazioni.
-
-    • Seleziona tutti gli ospiti che sono stati identificati con la carta di identità.
+    -- • Seleziona tutti gli ospiti che sono stati identificati con la carta di identità.
 Soluzione:
 ---
 SELECT *
 FROM ospiti
 WHERE document_type LIKE 'CI'
 ---
-    • Seleziona tutti gli ospiti che sono nati dopo il 1988.
+    -- • Seleziona tutti gli ospiti che sono nati dopo il 1988.
 Soluzione:
 ---
 SELECT *
 FROM ospiti
 WHERE YEAR(date_of_birth) > 1988
 ---
-    • Seleziona tutti gli ospiti che hanno più di 20 anni (al momento dell'esecuzione della query).
+    -- • Seleziona tutti gli ospiti che hanno più di 20 anni (al momento dell'esecuzione della query).
 Soluzione:
 ---
 SELECT *
 FROM ospiti
 WHERE YEAR(NOW()) - YEAR(date_of_birth) > 20
 ---
-    • Seleziona tutti gli ospiti il cui nome inizia con la D.
+    -- • Seleziona tutti gli ospiti il cui nome inizia con la D.
 Soluzione:
 ---
 SELECT *
 FROM ospiti
 WHERE name LIKE 'D%'
 ---
-    • Calcola il totale degli ordini accepted.
+    -- • Calcola il totale degli ordini accepted.
 Soluzione:
 ---
 SELECT status, SUM(price)
@@ -39,20 +39,20 @@ FROM pagamenti
 WHERE status LIKE 'accepted'
 GROUP BY status
 ---
-    • Qual è il prezzo massimo pagato?
+    -- • Qual è il prezzo massimo pagato?
 Soluzione:
 ---
 SELECT MAX(price)
 FROM pagamenti
 ---
-    • Seleziona gli ospiti riconosciuti con patente e nati nel 1975.
+    -- • Seleziona gli ospiti riconosciuti con patente e nati nel 1975.
 Soluzione:
 ---
 SELECT *
 FROM ospiti
 WHERE document_type LIKE 'Driver License' AND (YEAR(date_of_birth) = 1975)
 ---
-    • Quanti posti letto ha l'hotel in totale?
+    -- • Quanti posti letto ha l'hotel in totale?
 Soluzione:
 ---
 SELECT SUM(beds)
